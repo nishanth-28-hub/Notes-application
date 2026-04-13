b867007ed439b63ae5c3c8e47ccbaf3a903842af
nishanth-28-hub <nanth2810@gmail.com>
2026-04-13 19:40:08 +0530

    Update noteRoutes.js

--- src/routes/noteRoutes.js
+++ src/routes/noteRoutes.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { getNotes, createNote, trashNote } = require('../controllers/noteController');
+const { protect } = require('../middleware/authMiddleware');
 
+router.use(protect); // All note routes require login
+router.get('/', getNotes);
+router.post('/', createNote);
+router.put('/trash/:id', trashNote);
+
+module.exports = router;
