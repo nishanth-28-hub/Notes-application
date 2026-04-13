052f2d7f5b1e3dc362a4cbb205f188d2cc6c2fb9
nishanth-28-hub <nanth2810@gmail.com>
2026-04-13 19:53:18 +0530

    Delete src directory

--- src/models/Comment.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const CommentSchema = new mongoose.Schema({
-    note: { type: mongoose.Schema.Types.ObjectId, ref: 'Note', required: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    text: { type: String, required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Comment', CommentSchema);
--- src/routes/uploadRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const upload = require('../middleware/uploadMiddleware');
-const { uploadMedia } = require('../controllers/mediaController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/', protect, upload.single('media'), uploadMedia);
-
-module.exports = router;
