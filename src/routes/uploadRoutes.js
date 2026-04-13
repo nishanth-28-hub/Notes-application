dda18f92ca834b13acec0fa3067ff13683a0f803
nishanth-28-hub <nanth2810@gmail.com>
2026-04-13 19:40:24 +0530

    Update uploadRoutes.js

--- src/routes/uploadRoutes.js
+++ src/routes/uploadRoutes.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const upload = require('../middleware/uploadMiddleware');
+const { uploadMedia } = require('../controllers/mediaController');
+const { protect } = require('../middleware/authMiddleware');
 
+router.post('/', protect, upload.single('media'), uploadMedia);
+
+module.exports = router;
