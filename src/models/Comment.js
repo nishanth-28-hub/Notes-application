62de7bf9bee10f4489d1b0069c17924ac4ab0fbf
nishanth-28-hub <nanth2810@gmail.com>
2026-04-13 19:38:31 +0530

    Update Comment.js

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
 
+const CommentSchema = new mongoose.Schema({
+    note: { type: mongoose.Schema.Types.ObjectId, ref: 'Note', required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    text: { type: String, required: true },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Comment', CommentSchema);
