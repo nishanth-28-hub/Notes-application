f16ab376d1699a29ff6ccdb2660b2c5f1f931bc7
nishanth-28-hub <nanth2810@gmail.com>
2026-05-04 18:23:20 +0530

    Update NoteContext.js

--- frontend/src/context/NoteContext.js
+++ frontend/src/context/NoteContext.js
@@ -1,1 +1,45 @@
+import React, { createContext, useState } from 'react';
+import api from '../services/api';
 
+export const NoteContext = createContext();
+
+export const NoteProvider = ({ children }) => {
+  const [notes, setNotes] = useState([]);
+  const [activeNote, setActiveNote] = useState(null);
+
+  const fetchNotes = async () => {
+    const res = await api.get('/notes');
+    setNotes(res.data);
+  };
+
+  const addNote = async (noteData) => {
+    const res = await api.post('/notes', noteData);
+    setNotes([res.data, ...notes]);
+    setActiveNote(res.data);
+  };
+
+  const updateNote = async (id, data) => {
+    const res = await api.put(`/notes/${id}`, data);
+    setNotes(notes.map(n => n._id === id ? res.data : n));
+  };
+
+  const deleteNote = async (id) => {
+    await api.delete(`/notes/${id}`);
+    setNotes(notes.filter(n => n._id !== id));
+    if (activeNote?._id === id) setActiveNote(null);
+  };
+
+  const togglePin = async (id) => {
+    const res = await api.patch(`/notes/${id}/pin`);
+    setNotes(notes.map(n => n._id === id ? res.data : n));
+  };
+
+  return (
+    <NoteContext.Provider value={{ 
+      notes, activeNote, setActiveNote, fetchNotes, 
+      addNote, updateNote, deleteNote, togglePin 
+    }}>
+      {children}
+    </NoteContext.Provider>
+  );
+};
